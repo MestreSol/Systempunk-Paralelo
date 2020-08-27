9422511af6dc33a0f67c8f42b806692c25ee46fb
MestreSol <42494721+MestreSol@users.noreply.github.com>
2020-08-27 15:45:38 -0300

    Update
    Sistema de som OK.
    Classe GameConfig = Finals do jogo,
    Thread principal esta com erro nao informando

---
+++ Projeto/Jogo1/Jogo1.1/VersionAlpha/src/br/com/studio/lib/Sound.java
@@ -0,0 +1,83 @@
+package br.com.studio.lib;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound {
+
+	public static Sound main = new Sound("MainMusic.wav");
+
+	private AudioClip clip;
+	private Sound(String name) {
+		
+		try {
+	
+			clip = 	Applet.newAudioClip(Sound.class.getResource("/Sounds/"+name));
+	
+		}catch(Throwable e) {
+	
+			e.printStackTrace();
+		
+		}
+	}
+	
+	public void play() {
+	
+		try {
+		
+			new Thread() {
+			
+				public void run() {
+				
+					clip.play();
+				
+				}
+			}.start();
+		
+		}catch(Throwable e) {
+		
+			e.printStackTrace();
+		
+		}
+	}
+	
+	public void loop() {
+		
+		try {
+	
+			new Thread() {
+			
+				public void run() {
+				
+					clip.loop();
+				
+				}
+			}.start();
+		
+		}catch(Throwable e) {
+		
+			e.printStackTrace();
+		
+		}
+	}
+	
+	public void stop() {
+	
+		try {
+		
+			new Thread() {
+			
+				public void run() {
+				
+					clip.stop();
+		
+				}
+			}.start();
+	
+		}catch(Throwable e) {
+		
+			e.printStackTrace();
+		
+		}
+	}
+}
---
+++ Projeto/Jogo1/Jogo1.1/VersionAlpha/src/br/com/studio/main/GameConfig.java
@@ -0,0 +1,9 @@
+package br.com.studio.main;
+
+public class GameConfig {
+	public static final int WIDTH = 240;
+	public static final int HEIGHT = 160;
+	public static final int SCALE = 3;
+	public static boolean isRunning = false;
+	public static String GameState = "Menu";
+}
