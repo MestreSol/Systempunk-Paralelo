b8eb41eccd751aeade9add4fc50389cde01b8620
MestreSol <42494721+MestreSol@users.noreply.github.com>
2020-08-31 12:39:46 -0300

    Update Inputs.java
    Sistema de KeyListener pronto

--- Projeto/Jogo1/Jogo1.1/VersionAlpha/src/br/com/studio/lib/Inputs.java
+++ Projeto/Jogo1/Jogo1.1/VersionAlpha/src/br/com/studio/lib/Inputs.java
@@ -5,6 +5,8 @@ import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.studio.main.Game;
+
 public class Inputs implements KeyListener	 {
 	public class Key{
 		public int presses, absorbs;
@@ -30,16 +32,61 @@ public class Inputs implements KeyListener	 {
 		}
 	}
 	public List<Key> keys = new ArrayList<Key>();
+	public Key up = new Key();
+	public Key down = new Key();
+	public Key left = new Key();
+	public Key right = new Key();
+	public Key attack = new Key();
+	public Key menu = new Key();
+
+	
+	public void releaseAll() {
+		for(int i=0; i< keys.size(); i++) {
+			keys.get(i).down = false;
+		}
+	}
+	public void tick() {
+		for(int i = 0; i<keys.size();i++) {
+			keys.get(i).tick();
+		}
+	}
+	public Inputs(Game game) {
+		game.addKeyListener(this);
+	}
 	@Override
 	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		toggle(arg0, true);
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		toggle(arg0, false);
+	}
+	private void toggle(KeyEvent ke, boolean pressed) {
+		if(ke.getKeyCode() == KeyEvent.VK_NUMPAD8) up.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_NUMPAD2) down.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_NUMPAD4) left.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_NUMPAD6) right.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_W) up.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_S) down.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_A) left.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_D) right.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_UP) up.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_DOWN) down.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_LEFT) left.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_RIGHT) right.toggle(pressed);
+
+		if (ke.getKeyCode() == KeyEvent.VK_TAB) menu.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_ALT) menu.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_ALT_GRAPH) menu.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_SPACE) attack.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_CONTROL) attack.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_NUMPAD0) attack.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_INSERT) attack.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_ENTER) menu.toggle(pressed);
+
+		if (ke.getKeyCode() == KeyEvent.VK_X) menu.toggle(pressed);
+		if (ke.getKeyCode() == KeyEvent.VK_C) attack.toggle(pressed);
 	}
 
 	@Override
