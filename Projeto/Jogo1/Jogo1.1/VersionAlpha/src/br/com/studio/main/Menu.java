fcdca3c8b2081afe0625ffc50b1b8b6437dc0f71
MestreSol <42494721+MestreSol@users.noreply.github.com>
2020-09-01 08:54:51 -0300

    update
    Sistema de menu OK

--- Projeto/Jogo1/Jogo1.1/VersionAlpha/src/br/com/studio/main/Menu.java
+++ Projeto/Jogo1/Jogo1.1/VersionAlpha/src/br/com/studio/main/Menu.java
@@ -1,45 +1,52 @@
 package br.com.studio.main;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.util.List;
 
-import javax.imageio.ImageIO;
+import br.com.studio.lib.Color;
+import br.com.studio.lib.Font;
+import br.com.studio.lib.Inputs;
+import br.com.studio.lib.Screen;
+import br.com.studio.screen.ListItem;
 
 public class Menu {
-	public BufferedImage fundo;
-	public String[] options = {"New Game", "Loading","The Project", "Exit"};
-	public int currentOption = 0;
-	public int maxOption = options.length-1;
-	public boolean up = false;
-	public boolean down = false;
-	public GameConfig conf = new GameConfig();	
-	public Menu() {
-		try {
-			fundo = ImageIO.read(getClass().getResource("/FundoMenu.jpg"));
-		}catch(IOException e) {
-			e.printStackTrace();
-			System.out.println("E R R O: 0xFF000001");
-		}
+	protected Game game;
+	protected Inputs input;
+
+	public void init(Game game, Inputs input) {
+		this.input = input;
+		this.game = game;
 	}
+
 	public void tick() {
-		conf.isSaved();
-		if(up) {
-			currentOption--;
-			up = false;
-			if(currentOption < 0) {
-				currentOption = maxOption;
-			}
-		}else if(down) {
-			currentOption++;
-			down = false;
-			if(currentOption < 0) {
-				currentOption = maxOption;
-			}
-			if(currentOption >= 3) {
-				currentOption = 0;
-			}
+	}
+
+	public void render(Screen screen) {
+	}
+
+	public void renderItemList(Screen screen, int xo, int yo, int x1, int y1, List<? extends ListItem> listItems,
+			int selected) {
+		boolean renderCursos = true;
+		if (selected < 0) {
+			selected = -selected - 1;
+			renderCursos = false;
+		}
+		int w = x1 - xo;
+		int h = y1 - yo - 1;
+		int i0 = 0;
+		int i1 = listItems.size();
+		if (i1 > h)
+			i1 = h;
+		int io = selected - h / 2;
+		if(io > listItems.size() - h) io = listItems.size() - h;
+		if(io < 0) io = 0;
+		for(int i = i0; i<i1;i++) {
+			listItems.get(i +io).renderInventory(screen, (1+xo)*8,(i+1+yo)*8);
+		}
+		if(renderCursos) {
+			int yy = selected + 1 - io + yo;
+			Font.draw(">", screen, (xo + 0) * 8, yy * 8, Color.get(5, 555, 555, 555));
+			Font.draw("<", screen, (xo + w) * 8, yy * 8, Color.get(5, 555, 555, 555));
 		}
 	}
-	public void render() {}
-	
+
 }
---
+++ Projeto/Jogo1/Jogo1.1/VersionAlpha/src/br/com/studio/screen/ListItem.java
@@ -0,0 +1,7 @@
+package br.com.studio.screen;
+
+import br.com.studio.lib.Screen;
+
+public interface ListItem {
+	void renderInventory(Screen screen, int i, int j);
+}
